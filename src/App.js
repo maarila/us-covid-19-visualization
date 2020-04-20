c0a7c464c79304656edfcf5f63b6c6496dba7059
Mika Äärilä <maarila@gmail.com>
2020-04-20 19:05:48 +0300

    Add choropleth map of the US

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import * as d3 from 'd3';
 
-function App() {
+const UnitedStatesMap = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const width = 1280;
+    const height = 800;
+
+    const svg = d3.select(ref.current);
+
+    const projection = d3
+      .geoAlbersUsa()
+      .translate([width / 2, height / 2])
+      .scale([1500]);
+
+    const path = d3.geoPath().projection(projection);
+
+    d3.json('./us-state-boundaries.json').then(function(json) {
+      svg
+        .selectAll('path')
+        .data(json.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .style('stroke', '#888888')
+        .style('stroke-width', '2')
+        .style('fill', 'rgb(207,203,167)');
+    });
+  });
+
+  return <svg ref={ref} className="content" width="1280" height="800" />;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map-wrapper">
+      <UnitedStatesMap />
     </div>
   );
-}
+};
 
 export default App;
